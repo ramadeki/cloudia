c51aef3a34b47965c3a478f8fc9ba438b12de944
ramadeki <rama.deki33@gmail.com>
2025-10-19 20:29:24 +0700

    git pull

---
+++ main/footer.php
@@ -0,0 +1,40 @@
+  <!-- ------------------------------------- -->
+  <!-- Footer Start -->
+  <!-- ------------------------------------- -->
+  <footer>
+    <div class="container-fluid">
+      <div class="d-flex justify-content-between py-7 flex-md-nowrap flex-wrap gap-sm-0 gap-3">
+        <div class="d-flex gap-3 align-items-center">
+          <img src="../assets/images/logos/favicon.png" alt="icon">
+          <p class="fs-4 mb-0">All rights reserved by Modernize. </p>
+        </div>
+        <div>
+          <p class="mb-0">Produced by <a target="_blank" href="https://adminmart.com/" class="text-primary link-primary">AdminMart</a>.</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- ------------------------------------- -->
+  <!-- Footer End -->
+  <!-- ------------------------------------- -->
+
+  <!-- Scroll Top -->
+  <a href="javascript:void(0)" class="top-btn btn btn-primary d-flex align-items-center justify-content-center round-54 p-0 rounded-circle">
+    <i class="ti ti-arrow-up fs-7"></i>
+  </a>
+
+  <script src="../assets/js/vendor.min.js"></script>
+  <!-- Import Js Files -->
+  <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/libs/simplebar/dist/simplebar.min.js"></script>
+  <script src="../assets/js/theme/app.init.js"></script>
+  <script src="../assets/js/theme/theme.js"></script>
+  <script src="../assets/js/theme/app.min.js"></script>
+
+  <!-- solar icons -->
+  <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
+  <script src="../assets/libs/owl.carousel/dist/owl.carousel.min.js"></script>
+  <script src="../assets/js/theme/homepage.js"></script>
+</body>
+
+</html>
---
+++ main/header.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr" data-bs-theme="light" data-color-theme="Blue_Theme" data-layout="vertical">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <!-- Favicon icon-->
+  <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
+
+  <!-- Core Css -->
+  <link rel="stylesheet" href="../assets/css/styles.css" />
+
+  <title>Modernize Bootstrap Admin</title>
+  <!-- Owl Carousel  -->
+  <link rel="stylesheet" href="../assets/libs/owl.carousel/assets/owl.carousel.min.css" />
+
+<link rel="stylesheet" href="../assets/fonts/tabler-icons.woff2" />
+  
+</head>
+
+<body>
+
+
+  <!-- Preloader -->
+  <div class="preloader">
+    <img src="../assets/images/logos/favicon.png" alt="loader" class="lds-ripple img-fluid" />
+  </div>
+  <!-- ------------------------------------- -->
+  <!-- Header Start -->
+  <!-- ------------------------------------- -->
+  <header class="header-sticky">
+    <nav class="navbar navbar-expand-lg bg-primary-subtle py-2 py-lg-10">
+      <div class="custom-container d-flex align-items-center justify-content-between">
+        <a href="../main/dashboard.html" class="text-nowrap logo-img">
+          <img src="../assets/images/logos/dark-logo.svg" class="dark-logo" alt="Logo-Dark" />
+          <img src="../assets/images/logos/light-logo.svg" class="light-logo" alt="Logo-light" />
+        </a>
+        <button class="navbar-toggler border-0 p-0 shadow-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+          <i class="ti ti-menu-2 fs-8"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mx-auto mb-2 gap-xl-7 gap-8 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link fs-4 fw-bold text-dark link-primary" href="../main/frontend-aboutpage.html">Merchant</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active fs-4 fw-bold text-dark link-primary" href="../main/frontend-blogpage.html">Posting</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link fs-4 fw-bold text-dark link-primary d-flex gap-2" href="../main/frontend-portfoliopage.html">Report
+                <span class="badge text-primary bg-primary-subtle fs-2 fw-bolder hstack">New</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link fs-4 fw-bold text-dark link-primary" href="../main/index.html">Settings</a>
+            </li>
+          </ul>
+          <div>
+            <a href="../main/logout" class="tn mb-1 btn-primary rounded-circle round-40 btn-sm"><i class="ti ti-power fs-7"></i></a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <!-- ------------------------------------- -->
+  <!-- Header End -->
+  <!-- ------------------------------------- -->
+
+  <!-- ------------------------------------- -->
+  <!-- Responsive Sidebar Start -->
+  <!-- ------------------------------------- -->
+  <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+    <div class="offcanvas-header">
+      <a href="../main/frontend-landingpage.html">
+        <img src="../assets/images/logos/dark-logo.svg" alt="Logo-light" />
+      </a>
+      <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      <ul class="list-unstyled ps-0">
+        <li class="mb-1">
+          <a href="../main/frontend-aboutpage.html" class="px-0 fs-4 d-block text-dark link-primary w-100 py-2">
+            Merchant
+          </a>
+        </li>
+
+        <li class="mb-1">
+          <a href="../main/frontend-blogpage.html" class="px-0 fs-4 d-block w-100 py-2 text-dark link-primary">
+            Posting
+          </a>
+        </li>
+
+        <li class="mb-1">
+          <a href="../main/frontend-portfoliopage.html" class="px-0 fs-4 d-flex align-items-center justify-content-start gap-2 w-100 py-2 text-dark link-primary">
+            Portfolio
+            <span class="badge text-primary bg-primary-subtle fs-2 fw-bolder hstack">New</span>
+          </a>
+        </li>
+
+        <li class="mb-1">
+          <a href="../main/index.html" class="px-0 fs-4 d-block w-100 py-2 text-dark link-primary">
+            Dashboard
+          </a>
+        </li>
+
+        <li class="mb-1">
+          <a href="../main/frontend-pricingpage.html" class="px-0 fs-4 d-block w-100 py-2 text-dark link-primary">
+            Pricing
+          </a>
+        </li>
+
+        <li class="mb-1">
+          <a href="../main/frontend-contactpage.html" class="px-0 fs-4 d-block w-100 py-2 text-dark link-primary">
+            Contact
+          </a>
+        </li>
+        <li class="mt-3">
+          <a href="../main/authentication-login.html" class="btn btn-primary w-100">Log In</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <!-- ------------------------------------- -->
+  <!-- Responsive Sidebar End -->
+  <!-- ------------------------------------- -->
